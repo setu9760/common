d7397900d97ae16814cacab29ec6ea565e507e2c
Setu Desai <setu.desai@smartstream-stp.com>
2018-09-27 12:23:37 +0100

    commit

--- src/main/java/com/mycompany/app/common/service/ModelService.java
+++ src/main/java/com/mycompany/app/common/service/ModelService.java
@@ -1,10 +1,14 @@
 package com.mycompany.app.common.service;
 
+import com.mycompany.app.common.exception.RepositoryDataAccessException;
 import com.mycompany.app.common.model.ModelA;
 import com.mycompany.app.common.model.ModelB;
 
 public interface ModelService {
 
-	void save(ModelA a, ModelB b);
+	void save(ModelA a, ModelB b) throws RepositoryDataAccessException;
 	
+	ModelA getModelA(String name) throws RepositoryDataAccessException;
+	
+	ModelB getModelB(String name) throws RepositoryDataAccessException;
 }
